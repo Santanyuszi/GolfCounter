91d092638cd7ca0eb27994a7a710c5a7e76db77e
Zsolt Radvanyi <radvanyizsolt@gmail.com>
2017-12-09 00:18:58 +0100

    Reset animation added to PlayerScore TextViews

--- app/src/main/java/com/example/santanyuszi/golfcounter/MainActivity.java
+++ app/src/main/java/com/example/santanyuszi/golfcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.santanyuszi.golfcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Animation animScaleUp;
     Animation animScaleDown;
+    Animation animReset;
     int playerOneStroke = 0;
     int playerTwoStroke = 0;
 
@@ -26,15 +28,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void resetScore (View view) {
-        playerOneStroke = 0;
-        playerTwoStroke = 0;
-        displayForPlayerOne(playerOneStroke);
-        displayForPlayerTwo(playerTwoStroke);
-    }
-
     TextView firstPlayerScore = null;
     TextView secondPlayerScore = null;
+    Button resetScoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +41,14 @@ public class MainActivity extends AppCompatActivity {
                 R.anim.anim_scale);
         animScaleDown = AnimationUtils.loadAnimation(this,
                 R.anim.anim_scale_down);
+        animReset = AnimationUtils.loadAnimation(this,
+                R.anim.anim_reset);
 
 
 
         firstPlayerScore = (TextView) findViewById(R.id.player_one_stroke);
         secondPlayerScore = (TextView) findViewById(R.id.player_two_stroke);
-
+        resetScoreButton = (Button) findViewById(R.id.end_session);
 
         firstPlayerScore.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,11 +82,23 @@ public class MainActivity extends AppCompatActivity {
         secondPlayerScore.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                if (playerOneStroke >= 1)
+                if (playerTwoStroke >= 1)
                 playerTwoStroke = playerTwoStroke - 1;
                 displayForPlayerTwo(playerTwoStroke);
                 secondPlayerScore.startAnimation(animScaleDown);
                 return true;
+            }
+        });
+
+        resetScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerOneStroke = 0;
+                playerTwoStroke = 0;
+                displayForPlayerOne(playerOneStroke);
+                displayForPlayerTwo(playerTwoStroke);
+                firstPlayerScore.startAnimation(animReset);
+                secondPlayerScore.startAnimation(animReset);
             }
         });
 
